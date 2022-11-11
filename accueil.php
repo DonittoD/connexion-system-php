b07d2e9714d11f2a14135180eb9080905d9ed0c8
Daniel Feteira Do Couto <feiteira.daniel@hotmail.com>
2022-11-11 20:41:24 +0100

    correction session_start

--- accueil.php
+++ accueil.php
@@ -1,5 +1,6 @@
 <link href="styles/styles.css" rel="stylesheet">
 <?php 
+session_start();
     include ('includes/_header.php');
     echo 'ceci est l\'accueil';
     include ('includes/_footer.php');
